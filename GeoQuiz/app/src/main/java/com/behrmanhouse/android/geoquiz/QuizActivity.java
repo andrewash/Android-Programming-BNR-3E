dd28cfec36dce9781b7cffffb03c84a635aa9e7d
Andrew Ash <andrew@emotiveapps.com>
2017-06-29 10:47:28 -0700

    Finished Ch. 3

--- GeoQuiz/app/src/main/java/com/behrmanhouse/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/behrmanhouse/android/geoquiz/QuizActivity.java
@@ -1,7 +1,9 @@
 package com.behrmanhouse.android.geoquiz;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -10,11 +12,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mPrevButton;
-    private ImageButton mNextButton;
+    private View mNextButton;   // a Button on landscape, or ImageButton on portrait
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -31,7 +35,12 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
@@ -60,19 +69,21 @@ public class QuizActivity extends AppCompatActivity {
         });
 
 
-        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
-        mPrevButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mCurrentIndex == 0) {
-                    mCurrentIndex = mQuestionBank.length - 1;
-                } else {
-                    mCurrentIndex = mCurrentIndex - 1;
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+            mPrevButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mCurrentIndex == 0) {
+                        mCurrentIndex = mQuestionBank.length - 1;
+                    } else {
+                        mCurrentIndex = mCurrentIndex - 1;
+                    }
+                    updateQuestion();
                 }
-                updateQuestion();
-            }
-        });
-        mNextButton = (ImageButton) findViewById(R.id.next_button);
+            });
+        }
+        mNextButton = findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,8 +93,53 @@ public class QuizActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
     private void updateQuestion() {
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        Log.d(TAG, "Current question index: " + mCurrentIndex);
+        int question;
+        try {
+            question = mQuestionBank[mCurrentIndex].getTextResId();
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            // Log a message at "error" log level, along with an exception stack trace
+            Log.e(TAG, "Index out of bounds", ex);
+            return;
+        }
         mQuestionTextView.setText(question);
     }
 
